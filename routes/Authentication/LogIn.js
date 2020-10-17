3ddb47f0bd093a6888139491b3490abd48fe02de
Anh Le <kunquan2345@gmail.com>
2020-10-17 13:30:52 -0700

    Add simple routes structure

---
+++ routes/Authentication/LogIn.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function LogIn() {
+  return (
+    <View>
+      <Text>This is log in page</Text>
+    </View>
+  );
+}
