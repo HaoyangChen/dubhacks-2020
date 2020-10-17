c834b2aef16a86dd0c9d83b7a9d8ef8851ca672e
Haoyang Chen <haoyangchen0901@gmail.com>
2020-10-17 13:24:36 -0700

    Added React Native Paper, React Navigation, Starter Code

---
+++ screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import {
+  ImageBackground,
+  View,
+  StyleSheet,
+  Image,
+  Dimensions,
+} from "react-native";
+import * as React from "react";
+import TextButton from "../../components/TextButton";
+import colorData from "../../data/color.json";
+
+const width = Dimensions.get("window").width;
+const height = Dimensions.get("window").height;
+
+const HomeScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
