a34cad5409f092408798064bfcf550f3caec8594
Haoyang Chen <haoyangchen0901@gmail.com>
2020-10-17 14:13:29 -0700

    Added Stack Navigator

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
+import * as React from 'react';
+import { AppLoading } from 'expo';
+import * as Linking from 'expo-linking';
+import * as Font from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import { StyleSheet, Text, View } from 'react-native';
+import colorData from './data/color.json';
+import LoginScreen from './routes/Authentication/LoginScreen';
+
+const theme = {
+    ...DefaultTheme,
+    roundness: 2,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: colorData.brandColor.primary,
+        accent: colorData.brandColor.accent,
+    },
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
