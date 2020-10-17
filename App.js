6b696f5f5a444c71f7aaacbee390f7402c815386
Haoyang Chen <haoyangchen0901@gmail.com>
2020-10-17 15:03:09 -0700

    Adjusted App

--- App.js
+++ App.js
@@ -1,30 +1,15 @@
-import React from 'react';
-import * as firebase from 'firebase';
-import { AppLoading } from 'expo';
-import * as Linking from 'expo-linking';
-import * as Font from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import colorData from './data/color.json';
 
-import firebaseConfig from './config/firebase.config';
-import Routes from './routes/Routes';
-
-const theme = {
-    ...DefaultTheme,
-    roundness: 2,
-    colors: {
-        ...DefaultTheme.colors,
-        primary: colorData.brandColor.primary,
-        accent: colorData.brandColor.accent,
-    },
-};
-
-firebase.initializeApp(firebaseConfig);
-
-export default Routes;
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
 
 const styles = StyleSheet.create({
     container: {
