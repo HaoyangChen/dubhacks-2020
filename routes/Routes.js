28ab9eba3147d8d0a664df67a1ad302cdc8ceeb2
Haoyang Chen <haoyangchen0901@gmail.com>
2020-10-17 15:44:28 -0700

    Added React Native Theme

--- routes/Routes.js
+++ routes/Routes.js
@@ -5,19 +5,33 @@ import 'react-native-gesture-handler';
 
 import Authentication from './Authentication/Authentication';
 import Dashboard from './Dashboard/Dashboard';
+import colorData from '../data/color.json';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
 const Stack = createStackNavigator();
 
+const theme = {
+    ...DefaultTheme,
+    roundness: 2,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: colorData.brandColor.primary,
+        accent: colorData.brandColor.accent,
+    },
+};
+
 export default function Routes() {
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen
-                    name="Authentication"
-                    component={Authentication}
-                />
-                <Stack.Screen name="Dashboard" component={Dashboard} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <PaperProvider theme={theme}>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="Authentication"
+                        component={Authentication}
+                    />
+                    <Stack.Screen name="Dashboard" component={Dashboard} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </PaperProvider>
     );
 }
