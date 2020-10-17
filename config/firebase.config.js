01946ef123fccab0ea258fbf3339e52255f7d570
Anh Le <kunquan2345@gmail.com>
2020-10-17 16:48:45 -0700

    Add authentication to sign in page
    _ Only have for iOS and web, not android because android require SSL certificate
    _ TODO: set up some kind of connection after user sign in and firebase

--- config/firebase.config.js
+++ config/firebase.config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyA3TTZWeriB99_tJToB9JZQ5iSvo5Zf3GY",
-  authDomain: "dubhack-2020.firebaseapp.com",
-  databaseURL: "https://dubhack-2020.firebaseio.com",
-  projectId: "dubhack-2020",
-  storageBucket: "dubhack-2020.appspot.com",
-  messagingSenderId: "152788602514",
-  appId: "1:152788602514:web:a45d0eacaf8e9c2eb1204b",
-  measurementId: "G-KK2V5G1KE0",
+    apiKey: 'AIzaSyAgTxAHa2pTbxv9z-45kLXVEveJ-RFLI1M',
+    authDomain: 'dubhack-2020-768e6.firebaseapp.com',
+    databaseURL: 'https://dubhack-2020-768e6.firebaseio.com',
+    projectId: 'dubhack-2020-768e6',
+    storageBucket: 'dubhack-2020-768e6.appspot.com',
+    messagingSenderId: '526449658454',
+    appId: '1:526449658454:web:3249467363367acb5949f7',
+    measurementId: 'G-W4TET7Z4P4',
 };
