a2cb39c6a7d1a0e90d058c5269c101c62f53135e
Anh Le <kunquan2345@gmail.com>
2020-10-17 17:46:36 -0700

    Combine Google API with Firebase authentication
    _ Still only work for webapp and iOS
    _ After user successfully sign in with firebase we navigate user to Dashboard page
    _ TODO: check user for contact info and navigate user to that form if user haven't fill out the form yet

--- routes/Authentication/LogInRoute.js
+++ routes/Authentication/LogInRoute.js
@@ -17,7 +17,7 @@ export default function LogIn({ navigation }) {
                 googleConfig,
             );
             if (type === 'success') {
-                const { id, name, email } = user;
+                onSignIn(user, navigation, login);
             } else if (type === 'cancel') {
                 navigation.navigate('Register');
             }
@@ -32,4 +32,65 @@ export default function LogIn({ navigation }) {
             <Button title="Sign in with Google" onPress={signInWithGoogle} />
         </View>
     );
+}
+
+// This is how we manually handle the Google Sign-in flow
+// We pass the accessToken and idToken to firebase for checking if the user already authenticated with Firebase
+// If not we create a credential using firebase and user tokenId + accessToken
+// and use that credential to authenticate with firebase
+function onSignIn(googleUser, navigation, login) {
+    console.log('Google Auth Response', googleUser);
+    // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+    var unsubscribe = firebase
+        .auth()
+        .onAuthStateChanged(function (firebaseUser) {
+            unsubscribe();
+            // Check if we are already signed-in Firebase with the correct user.
+            if (!isUserEqual(googleUser, firebaseUser)) {
+                // Build Firebase credential with the Google ID token.
+                var credential = firebase.auth.GoogleAuthProvider.credential(
+                    googleUser.idToken,
+                    googleUser.accessToken,
+                );
+                // Sign in with credential from the Google user.
+                firebase
+                    .auth()
+                    .signInWithCredential(credential)
+                    .then((result) => {
+                        console.log(result);
+                        // Set the user in our global state
+                        login(googleUser.id);
+                        // Then navigate user to dashboard
+                        navigation.navigate('Dashboard');
+                    })
+                    .catch(function (error) {
+                        // Handle Errors here.
+                        var errorCode = error.code;
+                        var errorMessage = error.message;
+                        // The email of the user's account used.
+                        var email = error.email;
+                        // The firebase.auth.AuthCredential type that was used.
+                        var credential = error.credential;
+                        // ...
+                    });
+            } else {
+                console.log('User already signed-in Firebase.');
+            }
+        });
+}
+function isUserEqual(googleUser, firebaseUser) {
+    if (firebaseUser) {
+        var providerData = firebaseUser.providerData;
+        for (var i = 0; i < providerData.length; i++) {
+            if (
+                providerData[i].providerId ===
+                    firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+                providerData[i].uid === googleUser.getBasicProfile().getId()
+            ) {
+                // We don't need to re-auth the Firebase connection.
+                return true;
+            }
+        }
+    }
+    return false;
 }
