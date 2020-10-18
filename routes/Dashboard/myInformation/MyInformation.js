1c7b95e0d5a6ac57edc2ea4407e654eacc2452fe
Haoyang Chen <haoyangchen0901@gmail.com>
2020-10-17 23:09:41 -0700

    Added My Information Screen

--- routes/Dashboard/myInformation/MyInformation.js
+++ routes/Dashboard/myInformation/MyInformation.js
@@ -1,10 +1,58 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    Image,
+    ScrollView,
+    Dimensions,
+} from 'react-native';
+import * as firebase from 'firebase';
+import colorData from '../../../data/color.json';
+import { color } from 'react-native-reanimated';
+import ContainedIconButton from '../../../components/ContainedIconButton';
 
-export default function RegisterRoute() {
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
+
+const MyInformation = () => {
     return (
         <View>
-            <Text>Register route</Text>
+            <TouchableOpacity style={styles.cardBtnStyle}>
+                <Image
+                    source={require('../../../assets/myinformation/personalIcon.png')}
+                    style={styles.cardBtnIcon}
+                />
+                <Text style={styles.cardText}>Manage Appointments</Text>
+            </TouchableOpacity>
         </View>
     );
-}
+};
+
+const styles = StyleSheet.create({
+    cardBtnStyle: {
+        backgroundColor: colorData.brandColor.primaryLighter,
+        width: width,
+        height: Math.floor(width / 2 - 15),
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    cardBtnIcon: {
+        width: 60,
+        height: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    cardText: {
+        fontSize: 18,
+        fontFamily: Platform.OS === 'ios' ? 'Arial' : 'Roboto-Regular',
+        fontWeight: 'normal',
+        lineHeight: 31,
+        textAlign: 'center',
+        marginTop: 10,
+        color: colorData.neutralColor.lightest,
+    },
+});
+
+export default MyInformation;
